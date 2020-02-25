bf61f76ec81a9d58bad9bb8cd19e1467a429369c
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2020-02-25 13:36:46 +0100

    feat(workspaces): add some style

--- src/app/core/workspaces/workspaces.mock.ts
+++ src/app/core/workspaces/workspaces.mock.ts
@@ -2,7 +2,7 @@ export default [
   {
     id: '1',
     customer_id: '109281832182173912',
-    name: 'Blu',
+    name: 'Personale',
     collections: [
       {
         id: '11',
@@ -28,7 +28,7 @@ export default [
   {
     id: '2',
     customer_id: '109281832182173912',
-    name: 'Verde',
+    name: 'Lavoro',
     collections: [
       {
         id: '21',
@@ -49,14 +49,50 @@ export default [
       },
       {
         id: '22',
-        name: 'collection3',
+        name: 'Colori',
         tabs: [
           {
             id: '221',
-            link: 'http://bar.com',
-            icon: 'http://bar.com/image.png',
-            title: 'Bar bello',
-            description: 'Sito usato per determinare la bellezza di bar',
+            link: 'http://rosso.com',
+            icon: 'http://rosso.com/image.png',
+            title: 'Rosso',
+            description: 'Un sito rosso',
+            created: '2006-01-02T15:04:05Z07:00',
+            updated: '2006-01-02T15:04:05Z07:00'
+          },
+          {
+            id: '222',
+            link: 'http://verde.com',
+            icon: 'http://verde.com/image.png',
+            title: 'Verde',
+            description: 'Un sito verde',
+            created: '2006-01-02T15:04:05Z07:00',
+            updated: '2006-01-02T15:04:05Z07:00'
+          },
+          {
+            id: '223',
+            link: 'http://blu.com',
+            icon: 'http://blu.com/image.png',
+            title: 'Blu',
+            description: 'Un sito blu',
+            created: '2006-01-02T15:04:05Z07:00',
+            updated: '2006-01-02T15:04:05Z07:00'
+          },
+          {
+            id: '224',
+            link: 'http://giallo.com',
+            icon: 'http://giallo.com/image.png',
+            title: 'Giallo',
+            description: 'Un sito giallo',
+            created: '2006-01-02T15:04:05Z07:00',
+            updated: '2006-01-02T15:04:05Z07:00'
+          },
+          {
+            id: '225',
+            link: 'http://nero.com',
+            icon: 'http://nero.com/image.png',
+            title: 'Nero',
+            description: 'Un sito nero',
             created: '2006-01-02T15:04:05Z07:00',
             updated: '2006-01-02T15:04:05Z07:00'
           }
